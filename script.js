097613bafbb71d1ea40210982b193b55c389d4bd
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-12-11 14:26:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+const svg = d3.select("#worldmap");
+const width = +svg.attr("width");
+const height = +svg.attr("height");
+let toolTip=d3.select("#tooltip");
+const projection = d3.geoNaturalEarth1()
+      .translate([width / 2.35, height / 3])
+      .scale(90);
+let arr=
+[{"id":1,"country":"Indonesien","city":"Jakarta","lon":106.85,"lat":-6.21,"population":11634100,"tz":"Asia/Jakarta"},
+{"id":2,"country":"Myanmar","city":"Mandalay","lon":96.05,"lat":21.85,"population":1594300,"tz":"Asia/Rangoon"},
+{"id":3,"country":"Kasachstan","city":"Almaty","lon":76.93,"lat":43.25,"population":2042040,"tz":"Asia/Almaty"},
+{"id":4,"country":"Iran","city":"Isfahan","lon":51.67,"lat":32.65,"population":2327990,"tz":"Asia/Tehran"},
+{"id":5,"country":"Israel","city":"Haifa","lon":34.99,"lat":32.79,"population":1199980,"tz":"Asia/Hebron"},
+{"id":6,"country":"Bosnien","city":"Sarajevo","lon":18.41,"lat":43.85,"population":348404,"tz":"Europe/Sarajevo"},
+{"id":7,"country":"Slowenien","city":"Ljubljana","lon":14.51,"lat":46.05,"population":291140,"tz":"Europe/Ljubljana"},
+{"id":8,"country":"Österreich","city":"Graz","lon":15.45,"lat":47.07,"population":305314,"tz":"Europe/Vienna"},
+{"id":9,"country":"England","city":"Brighton", "lon":-0.17,"lat":50.83,"population":290885,"tz":"Europe/London"},
+{"id":10,"country":"Spanien","city":"Granada","lon":-3.61,"lat":37.19,"population":234,325,"tz":"Europa/Madrid"},
+{"id":11,"country":"Italien","city":"Catania","lon":15.08,"lat":37.50,"population":297517,"tz":"Europe/Rom"},{"id":12,"country":"Algerien","city":"Algier","lon":3.22,"lat":36.68,"population":2364230,"tz":"Africa/Algiers"},
+{"id":13,"country":"Mauretanien","city":"Nouakchott","lon":-15.98,"lat":18.09,"population":1612940,"tz":"Africa/Nouakchott"},
+{"id":14,"country":"Burkina Faso","city":"Ouagadougou","lon":-1.53,"lat":12.37,"population":3520820,"tz":"Africa/Ouagadougou"},
+{"id":15,"country":"Repubik Kongo","city":"Brazzaville","lon":15.28,"lat":-4.27,"population":2813480,"tz":"Africa/Brazzaville"},
+{"id":16,"country":"Chile","city":"Valparaíso", "lon":-71.63,"lat":-33.03,"population":1024430,"tz":"America/Santiago"},
+{"id":17,"country":"Kolumbien","city":"Medellín","lon":-75.56,"lat":6.25,"population":4172810,"tz":"America/Bogota"},
+{"id":18,"country":"El Salvador","city":"San Salvador","lon":-89.20,"lat":13.72,"population":1132420,"tz":"America/El_Salvador"},
+{"id":19,"country":"USA","city":"Santa Fe","lon":-105.94,"lat":35.69,"population":91254,"tz":"America/Los_Angeles"}];
+
+    const path = d3.geoPath().projection(projection);
+
+    // Daten laden (TopoJSON -> GeoJSON)
+    d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json")
+      .then(worldData => {
+        const countries = topojson.feature(worldData, worldData.objects.countries).features;
+
+        svg.append("g")
+          .selectAll("path")
+          .data(countries)
+          .join("path")
+          .attr("d", path)
+          .attr("fill", "#d9c9a3")   // sandiges Land
+          .attr("stroke", "#333") // dezente Grenzen
+          .attr("stroke-width", 0.5);   
+      let mouseover=(d,i)=>{
+     toolTip.style("visibility","visible").style("top",event.pageY-30+"px").style("left",width/2+20+"px").html("Land: "+i["country"]+"<br>"+"Stadt: "+i["city"]+"<br>"+"Einwohner: "+i["population"]+"<br><br>"+"Datum : "+new Date().toLocaleDateString('de-DE',{timeZone:i["tz"]})+"<br>"+"Uhrzeit: "+new Date().toLocaleTimeString('de-DE',{timeZone:i["tz"]}));
+      }
+        svg.selectAll("circle").data(arr).enter().append("circle").attr("cx",(item)=>projection([item["lon"],item["lat"]])[0]).attr("cy",(item)=>projection([item["lon"],item["lat"]])[1]).attr("r",2).attr("fill","#714342").attr("stroke","#000").attr("opacity",0.7).attr("stroke-width",0.5).on("mouseover",mouseover).on("mouseleave",()=>{toolTip.style("visibility","hidden")});
+      })
+      .catch(err => {
+        console.error("Fehler beim Laden der Weltkarte:", err);
+      });
